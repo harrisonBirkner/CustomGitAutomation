54361252f6e32333ace60e3651aeffce8f7d3824
harrisonBirkner <harrison.birkner@gmail.com>
2020-11-21 17:43:49 -0600

    Added functionality for custom repo description

--- GitRepo2Helper.bat
+++ GitRepo2Helper.bat
@@ -1,11 +1,12 @@
 @echo off
 
 set newRepoName=%1
+set repoDesc=%2
 
 cd C:/
 md %newRepoName%
 cd %newRepoName%
-echo %newRepoName%>readme.txt
+echo %repoDesc%>readme.txt
 git init
 git remote add origin https://github.com/harrisonBirkner/%newRepoName%
 git add .
