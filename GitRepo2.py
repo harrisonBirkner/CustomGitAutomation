841329930a6d389af5ad568d63b367bf2ee2c9f8
harrisonBirkner <harrison.birkner@gmail.com>
2020-11-21 23:14:31 -0600

    Python better compartmentalized

--- GitRepo2.py
+++ GitRepo2.py
@@ -3,30 +3,42 @@ import subprocess
 from selenium import webdriver
 import time
 
-username = 'harrisonBirkner'
-password = 'GizkaFruit2402'
-envList = ['v', 'c', 'j']
-newRepoName = input('Repository Name: ')
-repoDesc = input('Repository Description: ')
-envPick = input('Environment (v, c or j): ')
-
-while envPick not in envList:
+def repoInput():
+    envList = ['v', 'c', 'j']
+    newRepoName = input('Repository Name: ')
+    repoDesc = input('Repository Description: ')
     envPick = input('Environment (v, c or j): ')
 
-browser = webdriver.Chrome(executable_path='C:/chromedriver.exe')
-browser.get('http://github.com/login')
+    while envPick not in envList:
+        envPick = input('Environment (v, c or j): ')
 
-browser.find_elements_by_xpath("//input[@name='login']")[0].send_keys(username)
-browser.find_elements_by_xpath("//input[@name='password']")[0].send_keys(password)
-browser.find_elements_by_xpath("//input[@name='commit']")[0].click()
-browser.find_elements_by_xpath("/html/body/div[4]/div/aside[1]/div[2]/div[1]/div/h2/a")[0].click()
-browser.find_element_by_xpath("//input[@id='repository_name']").send_keys(newRepoName)
-browser.find_element_by_xpath("//input[@id='repository_description']").send_keys(repoDesc)
-browser.find_element_by_xpath("//input[@id='repository_visibility_private']").click()
-time.sleep(.5)
-browser.find_elements_by_class_name("btn-primary")[0].click()
+    return newRepoName, repoDesc, envPick
 
-print("Succesfully created repository {}".format(newRepoName))
+def repoCreation(newRepoName, repoDesc, envPick):
+    username = 'harrisonBirkner'
+    password = 'GizkaFruit2402'
 
-item = subprocess.Popen([r"C:\\CustomGitAutomation\\GitRepo2Helper.bat", newRepoName, repoDesc, envPick] , 
-                         shell=True)
+    browser = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+    browser.get('http://github.com/login')
+
+    browser.find_elements_by_xpath("//input[@name='login']")[0].send_keys(username)
+    browser.find_elements_by_xpath("//input[@name='password']")[0].send_keys(password)
+    browser.find_elements_by_xpath("//input[@name='commit']")[0].click()
+    browser.find_elements_by_xpath("/html/body/div[4]/div/aside[1]/div[2]/div[1]/div/h2/a")[0].click()
+    browser.find_element_by_xpath("//input[@id='repository_name']").send_keys(newRepoName)
+    browser.find_element_by_xpath("//input[@id='repository_description']").send_keys(repoDesc)
+    browser.find_element_by_xpath("//input[@id='repository_visibility_private']").click()
+    time.sleep(.5)
+    browser.find_elements_by_class_name("btn-primary")[0].click()
+
+def main():
+    newRepoName, repoDesc, envPick = repoInput()
+
+    repoCreation(newRepoName, repoDesc, envPick)
+
+    print("Succesfully created repository {}".format(newRepoName))
+
+    item = subprocess.Popen([r"C:\\CustomGitAutomation\\GitRepo2Helper.bat", newRepoName, repoDesc, envPick] , 
+                             shell=True)
+
+main()
